9aa42f029d01ab8c6dacea69c81ce1b85bdff625
amigobro757 <151609301+amigobro757@users.noreply.github.com>
2023-11-21 17:19:53 +1000

    Add files via upload
    ты думал тут что-т будет? ууу чел......

---
+++ helloworld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helloworld : MonoBehaviour
+{
+    
+    void Start()
+    {
+        // выводит надпись привет мир
+        print("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
